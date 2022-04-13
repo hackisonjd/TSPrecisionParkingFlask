852555b1eab80b12e3d5f4ea1f97b6df9c2cef9c
jackson@heehoposting.xyz <jackson@heehoposting.xyz>
2022-04-13 17:45:37 -0400

    Comments and requirements file

--- website/models.py
+++ website/models.py
@@ -1,6 +1,7 @@
 from . import db
 from sqlalchemy.sql import func
 
+# Stores the location of each sensor.
 class Location(db.Model):
     location_id = db.Column(db.Numeric(1,1, asdecimal=False), nullable=False, primary_key=True)
     location_name = db.Column(db.String(50))
@@ -8,12 +9,14 @@ class Location(db.Model):
     latitude = db.Column(db.Numeric(8,5))
     sensors = db.relationship('Sensor')
 
+# Data for each individual sensor.
 class Sensor(db.Model):
     sensor_id = db.Column(db.Numeric(1,1, asdecimal=False), nullable=False, primary_key=True)
     start_time = db.Column(db.DateTime(timezone=True), default=func.now())
     location_id = db.Column(db.Numeric(asdecimal=False), db.ForeignKey('location.location_id'))
     records = db.relationship('Record')
 
+# Record data for each sensor, with accurate timelogs.
 class Record(db.Model):
     reading_id = db.Column(db.Numeric(1,1, asdecimal=False), nullable=False, primary_key=True)
     reading = db.Column(db.Boolean())
