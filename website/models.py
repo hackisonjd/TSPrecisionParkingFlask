487101888c03e1a23e950821696e238a4b6f37eb
jackson <jackson@heehoposting.xyz>
2022-04-14 02:39:41 -0400

    Added functionality to update database from front-end.

--- website/models.py
+++ website/models.py
@@ -3,7 +3,7 @@ from sqlalchemy.sql import func
 
 # Stores the location of each sensor.
 class Location(db.Model):
-    location_id = db.Column(db.Numeric(1,1, asdecimal=False), nullable=False, primary_key=True)
+    location_id = db.Column(db.Integer().with_variant(db.Integer, "sqlite"), primary_key=True)
     location_name = db.Column(db.String(50))
     longitude = db.Column(db.Numeric(8,5))
     latitude = db.Column(db.Numeric(8,5))
@@ -11,14 +11,17 @@ class Location(db.Model):
 
 # Data for each individual sensor.
 class Sensor(db.Model):
-    sensor_id = db.Column(db.Numeric(1,1, asdecimal=False), nullable=False, primary_key=True)
+    sensor_id = db.Column(db.Integer(), primary_key=True)
     start_time = db.Column(db.DateTime(timezone=True), default=func.now())
     location_id = db.Column(db.Numeric(asdecimal=False), db.ForeignKey('location.location_id'))
     records = db.relationship('Record')
 
+    def __repr__(self):
+        return '<Sensor %r>' % self.sensor_id
+
 # Record data for each sensor, with accurate timelogs.
 class Record(db.Model):
-    reading_id = db.Column(db.Numeric(1,1, asdecimal=False), nullable=False, primary_key=True)
+    reading_id = db.Column(db.Integer(), primary_key=True)
     reading = db.Column(db.Boolean())
     record_timestamp = db.Column(db.DateTime(timezone=True), default=func.now())
     sensor_id = db.Column(db.Numeric(asdecimal=False), db.ForeignKey('sensor.sensor_id'))
--- website/views.py
+++ website/views.py
@@ -1,11 +1,19 @@
-from flask import Flask, Blueprint, redirect, url_for, render_template
+from flask import Flask, Blueprint, redirect, url_for, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from .models import Location, Sensor, Record
+from . import db
 
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("index.html")
+    if request.method == 'POST':
+        if request.form['deploy_button'] == 'Deploy Sensor':
+            sensor = Sensor()
+            db.session.add(sensor)
+            db.session.commit()
+
+    return render_template('index.html')
 
 @views.route("/admin")
 def admin():
